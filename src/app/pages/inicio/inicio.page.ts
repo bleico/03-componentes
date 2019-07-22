df2b088fe9f9a66c9277ba3b08a669af971bb0f0
Bleider <bleiderc@gmail.com>
2019-07-22 06:42:04 -0400

    Primer commit
    Este es el primer commit del proyecto

---
+++ src/app/pages/inicio/inicio.page.ts
@@ -0,0 +1,71 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-inicio",
+  templateUrl: "./inicio.page.html",
+  styleUrls: ["./inicio.page.scss"]
+})
+export class InicioPage implements OnInit {
+  componentes: Componente[] = [
+    {
+      icon: "american-football",
+      name: "Action Sheet",
+      redirecTo: "/action-sheet"
+    },
+    {
+      icon: "appstore",
+      name: "Alert",
+      redirecTo: "/alert"
+    },
+    {
+      icon: "beaker",
+      name: "Avatar",
+      redirecTo: "/avatar"
+    },
+    {
+      icon: "radio-button-on",
+      name: "Botones y router",
+      redirecTo: "/botones"
+    },
+    {
+      icon: "card",
+      name: "Cards",
+      redirecTo: "/card"
+    },
+    {
+      icon: 'checkmark-circle-outline',
+      name: 'Checkbox',
+      redirecTo: '/check'
+    },
+    {
+      icon: 'calendar',
+      name: 'DateTime',
+      redirecTo: '/date-time'
+    },
+    {
+      icon: 'car',
+      name: 'Fabs',
+      redirecTo: '/fab'
+    },
+    {
+      icon: 'grid',
+      name: 'Grid - Rows',
+      redirecTo: '/grid'
+    },
+    {
+      icon: 'infinite',
+      name: 'Infinite Scroll',
+      redirecTo: '/infinite-scroll'
+    }
+  ];
+
+  constructor() {}
+
+  ngOnInit() {}
+}
+
+interface Componente {
+  icon: string;
+  name: string;
+  redirecTo: string;
+}
