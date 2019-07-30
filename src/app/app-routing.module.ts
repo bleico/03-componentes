b46a1294f1de2d81f3a6d9e0af7b94dcd793f06f
Bleider <bleiderc@gmail.com>
2019-07-29 22:32:24 -0400

    Agregando tabs

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -26,7 +26,8 @@ const routes: Routes = [
   { path: 'refresher', loadChildren: './pages/refresher/refresher.module#RefresherPageModule' },
   { path: 'progress-bar', loadChildren: './pages/progress-bar/progress-bar.module#ProgressBarPageModule' },  { path: 'searchbar', loadChildren: './pages/searchbar/searchbar.module#SearchbarPageModule' },
   { path: 'segment', loadChildren: './pages/segment/segment.module#SegmentPageModule' },
-  { path: 'slides', loadChildren: './pages/slides/slides.module#SlidesPageModule' }
+  { path: 'slides', loadChildren: './pages/slides/slides.module#SlidesPageModule' },
+  { path: 'tabs', loadChildren: './pages/tabs/tabs.module#TabsPageModule' }
 
 ];
 
